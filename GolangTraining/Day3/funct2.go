d95846577c6f929a9590a9cd839fc2221d4f7b1e
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-01-06 15:55:50 +0530

    func returning a func

--- GolangTraining/Day3/funct2.go
+++ GolangTraining/Day3/funct2.go
@@ -1,11 +1,19 @@
 package main
 
 import "fmt"
+// import "reflect"
 
 func main(){
 	fnreturned:= test2()
 	fnreturned() 
-	//this function is getting invoked
+    //this function is getting invoked
+
+	// test2()() this can also be directly used instead of above two statements
+
+
+	// fmt.Println(test1) returns address of test1 -example 0xe8c660
+	// fmt.Println(reflect.TypeOf(test1)) returns func() 
+	
 }
 
 func test1(){
