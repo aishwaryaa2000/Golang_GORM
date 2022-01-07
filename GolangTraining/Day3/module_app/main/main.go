4a71a1986c3aa6e7cb207686af7407037537bbd5
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-01-07 15:29:35 +0530

    .

--- GolangTraining/Day3/module_app/main/main.go
+++ GolangTraining/Day3/module_app/main/main.go
@@ -5,3 +5,25 @@ import "module_app/greeting/hindi"
 func main()  {
 	hindi.SayHello()
 }
+
+
+// go mod init inside module_app  --- to be done only once
+// C:\xampp\htdocs\Swabhav_Techlabs\GolangTraining\Day3\module_app>go get github.com/headfirstgo/greeting
+
+// package main
+
+// import "github.com/headfirstgo/greeting"
+
+// func main()  {
+// 	greeting.Hi()
+// }
+
+
+
+// package main
+
+// import "module_app/greeting"
+
+// func main()  {
+// 	greeting.SayHello()
+// }
