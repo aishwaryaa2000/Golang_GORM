a72367669729433788d7237dfe0f5b3e12d406d5
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-01-07 16:42:16 +0530

    Array introduction

--- GolangTraining/Day4/array.go
+++ GolangTraining/Day4/array.go
@@ -4,5 +4,18 @@ import "fmt"
 
 func main() {
 	var myArray = [3]int{10, 20, 30}
+
+	// 	var myArray = [...]int{10, 20} 
+	//  ... automatically declares the size of array acc to the no. of elements present ,above case size = 2
+	
 	fmt.Println(myArray)
+
+	// array is call by value,so if we assign it to another variable then A COPY OF THE ARRAY is saved
+	//if we change anything in the COPY OF ARRAY then ORIGINAL ARRAY WON'T CHANGE
+	
+	var copyMyArray = myArray
+	copyMyArray[0] = 40
+	fmt.Println("My array is : ",myArray)
+	fmt.Println("Copy of array : ",copyMyArray)
+
 }
