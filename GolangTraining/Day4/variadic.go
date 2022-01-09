450846ff6d1bab30493cfa4e457c6b0d99f80642
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-01-09 19:30:15 +0530

    Variadic parameters

---
+++ GolangTraining/Day4/variadic.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println(sum(""))
+	fmt.Println(sum("",10))
+	fmt.Println(sum("",10,20,30,40))
+
+	var mySlice = []int{10,20,30,40}
+	fmt.Println(reflect.TypeOf(mySlice))
+	fmt.Println(sum("",mySlice...))
+	// mySlice has variable arguments inside it or many values inside it so three dots... while func call
+
+
+
+}
+
+func sum(str string,num...int) int{
+	total := 0
+	for _,value := range num{
+		total += value
+	}
+
+	return total
+}
+
+// variable number of arguments
+// first we need to type the compulsory parameters and then the variadic variables with ...
