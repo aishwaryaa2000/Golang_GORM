ec580dc570ce28273c5afa7918dae9788de58fde
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-03-14 12:36:59 +0530

    Comments added

--- GolangTraining/goAndJson/Mux/main.go
+++ GolangTraining/goAndJson/Mux/main.go
@@ -38,22 +38,23 @@ type Address struct{
 
 func main(){
 	r := mux.NewRouter()
-	r.HandleFunc("/createEMP",createEMP).Methods("POST")
+	r.HandleFunc("/createEMP",createEMP).Methods("POST") 
+	// POST is used to send data to a server to create/update a resource. 
+	// The data sent to the server with POST is stored in the request body of the HTTP request
 	fmt.Println("Sever started")
 	http.ListenAndServe(":9090",r)
 }
 func createEMP(w http.ResponseWriter,r *http.Request){
-	// jsonFile,err1 := os.Open("userData.json")
-	// if err1!=nil{
-	// 	fmt.Println(err1)
-	// }
+	//r is request from client to server
 	var mappedData Users
-	jsonByteData,err2 := ioutil.ReadAll(r.Body)
+	jsonByteData,err2 := ioutil.ReadAll(r.Body) 
+	//r.Body is the request body given from client to server
 	if err2!=nil{
 		fmt.Println(err2)
 	}
 	json.Unmarshal(jsonByteData,&mappedData)
-
+	//Displaying the data given by the client 
+	//Here,the data is given by postman POST method under which body consists of JSON data
 	for _,iUser := range mappedData.U{
 		fmt.Println("Name : ",iUser.UserName)
 		fmt.Println("Phone numbers \n    Home number",iUser.UserPhNumber.HomeNo,"\n    Office number : ",iUser.UserPhNumber.OfficeNo)
