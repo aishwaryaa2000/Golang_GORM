1107a71ecd181fa2514d6359a35f779d220cb13a
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-03-17 14:16:49 +0530

    Templates

---
+++ GolangTraining/goAndJson/templateGo/Employee.go
@@ -0,0 +1,11 @@
+package main
+
+type Employee struct{
+
+        EmpName string `json:"EmpName"`
+
+        EmailID int `json:"EmailID"`
+
+        EmpSal float32 `json:"EmpSal"`
+
+}
---
+++ GolangTraining/goAndJson/templateGo/Emptemp.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type properties struct{
+	Name string
+	Dtype string
+}
+
+type newStruct struct{
+	StructName string
+	StructProperty []properties
+}
+
+func createMicroservice(w http.ResponseWriter,r *http.Request){
+	props := []properties{
+							{Name : "EmpName", Dtype : "string"},
+							{Name : "EmailID", Dtype: "int"},
+							{Name : "EmpSal",Dtype: "float32"},
+						}
+
+	emp := newStruct{
+						StructName: "Employee",
+						StructProperty: props,
+					}
+	
+	templatePath := "C:\\xampp\\htdocs\\Swabhav_Techlabs\\GolangTraining\\goAndJson\\templateGo\\structTemplate.txt"
+
+	tmpl,err:= template.New("structTemplate.txt").ParseFiles(templatePath)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	filename := emp.StructName + ".go"
+	f,err := os.Create(filename)
+
+	if err!=nil{
+		log.Println("Create file error : ",err)
+		return
+	}
+	tmpl.Execute(f,emp)
+}
+
+func initrouter(){
+	r := mux.NewRouter()
+	r.HandleFunc("/createMS",createMicroservice).Methods("POST")
+	log.Fatal(http.ListenAndServe(":9090",r))
+}
+
+func main(){
+	initrouter()
+}
