bb8d9fc32f4a4b18adc20deec64fad4891b09b13
Anand <Aishwarya.Anand@forcepoint.com>
2022-07-08 12:05:52 +0530

    How to break outer for

--- GolangTraining/Day2/loops.go
+++ GolangTraining/Day2/loops.go
@@ -26,9 +26,17 @@ func main(){
 		println("Sum value : ",sum)
 	}
 
-
 }
 
 
 
 // Only FOR loop in Golang
+//to break outer for loop you can use FOR LABEL
+//myFor:
+//for i:=0;i<10;i++{
+//	for j:=0;j<10;j++{
+//		 if j==1{
+//			 break myFor
+//       }
+//    }
+//}
