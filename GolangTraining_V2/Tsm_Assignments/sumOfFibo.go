90efcdfe4602912641f5901f46e1475bd376adea
Anand <Aishwarya.Anand@forcepoint.com>
2022-07-10 16:35:16 +0530

    Sum of fibonacci series upto nth term

--- GolangTraining_V2/Tsm_Assignments/sumOfFibo.go
+++ GolangTraining_V2/Tsm_Assignments/sumOfFibo.go
@@ -1,29 +1,35 @@
 package main
 
-import(
+import (
+	"errors"
 	"fmt"
 	"log"
 )
 
-func sumOfFibonaci(noOfTerms int) uint64 {
-	//0 1 1 2 3 8...
-	if noOfTerms==1{
-		return 0
-	}else if noOfTerms==2{
-		return 1
+func sumOfFibonaci(noOfTerms int) (uint64,error) {
+	//Fibonacci series : 0 1 1 2 3 8...
+	if noOfTerms<0{
+		err := errors.New("Number of terms cannot be less than zero")
+		return 0,err
+	}
+	if noOfTerms==1 || noOfTerms==0 {
+		return 0,nil
 	}else{
 		var(
+			//Taking uint64 because we know that the numbers will always be positive thereby increasing the range
 			sumTotal, firstNum, secondNum,thirdNum uint64= 1,0,1,0
 		  )
 
+		fmt.Print("Fibonacci series : ",firstNum," ",secondNum," ")
 		for i:=0;i<noOfTerms-2;i++{
 			thirdNum = firstNum+secondNum
+			fmt.Print(thirdNum," ")
 			sumTotal += thirdNum
 			firstNum=secondNum
 			secondNum=thirdNum
 		}
 
-		return sumTotal
+		return sumTotal,nil
 
 	}
 	
@@ -38,6 +44,11 @@ func main(){
 		//Example-If user enters an alphabet then raise an 
 		log.Fatal(err)
 	}	
-	fmt.Println("Sum of fibonaci series upto ",no," terms is : ",sumOfFibonaci(no))
+	sum,err := sumOfFibonaci(no)
+	if err!=nil{
+		fmt.Println("Error occured : ",err)
+	}else{
+		fmt.Println("\nSum of fibonaci series upto ",no," terms is : ",sum)
+	}
 
 }
