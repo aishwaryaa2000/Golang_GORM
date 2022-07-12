a1ec94b776499531020d2e9de26b988ad5041fc6
Anand <Aishwarya.Anand@forcepoint.com>
2022-07-12 13:39:06 +0530

    battleship

--- GolangTraining_V2/BattleShip/service/gameService.go
+++ GolangTraining_V2/BattleShip/service/gameService.go
@@ -8,5 +8,7 @@ import (
 func Board(rowSizeByUser,colSizeByUser uint8) *board.Board{
 	board := board.New(rowSizeByUser,colSizeByUser)
 	board.Display()
+	board.BoardInit()
+	board.Display()
 	return board
 }
