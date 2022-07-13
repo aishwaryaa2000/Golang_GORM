7905218d888ce0dd8a1b3094da12ccd60dc1fc3e
Anand <Aishwarya.Anand@forcepoint.com>
2022-07-13 11:09:08 +0530

    Sorting

---
+++ GolangTraining_V2/Tsm_Assignments/sorting.go
@@ -0,0 +1,60 @@
+/* 
+	Insertion sort 
+	O(n^2)
+
+	Merge sort can be used as time complexity is O(n logn) but it uses stack 
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	numberSlice := make([]int,0,10)
+    var len,i uint64
+	var userNumber int
+	fmt.Print("Enter the count of numbers you wish to enter : ")
+	_,err := fmt.Scanln(&len)
+
+	if err!=nil{
+		//If user enters a character i.e not a number and when user enters a negative number as len is uint
+		fmt.Println("Either you entered a negative number or a character")
+		log.Fatal(err)
+	}
+
+    for i=0;i<len;i++{
+		fmt.Print("Enter number",i+1,": ")
+		_,err2:=fmt.Scanln(&userNumber)
+		if err2!=nil{
+			//If user enters a character i.e not a number
+			log.Fatal(err2)
+		}
+		numberSlice = append(numberSlice,userNumber)
+	}
+
+	sorting(numberSlice)
+	fmt.Println("\nAfter sorting the array : ",numberSlice)
+	//Slice is always call by ref so changes are reflected
+
+}
+
+func sorting(number []int){
+	var j int
+	var temp int
+    for i := 1;i< len(number); i++ {
+        temp = number[i]
+        j = i - 1
+
+        for ; j >= 0 && number[j] > temp ;{
+            number[j + 1] = number[j]
+            j--
+			fmt.Println(j)
+        }
+        number[j + 1] = temp
+
+    }
+
+}
